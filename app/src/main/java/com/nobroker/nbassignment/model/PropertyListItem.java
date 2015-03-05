e40d5bc331264a42d5f684e893d1aec76d07ba98
sachingajraj <sachingajraj@gmail.com>
2015-03-06 01:25:20 +0530

    property list item modal & adaptor

--- app/src/main/java/com/nobroker/nbassignment/model/PropertyListItem.java
+++ app/src/main/java/com/nobroker/nbassignment/model/PropertyListItem.java
@@ -1,4 +1,52 @@
 package com.nobroker.nbassignment.model;
 
+import java.util.ArrayList;
+
 public class PropertyListItem {
+    private String title, thumbnailUrl;
+    private String rent;
+    private String size;
+
+    public PropertyListItem() {
+    }
+
+    public PropertyListItem(String name, String thumbnailUrl, double rent, double size,
+                 ArrayList<String> genre) {
+        this.title = name;
+        this.thumbnailUrl = thumbnailUrl;
+        this.rent = Double.toString(rent);
+        this.size = Double.toString(size);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String name) {
+        this.title = name;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public String getRent() {
+        return rent;
+    }
+
+    public void setRent(double rent) {
+        this.rent = Double.toString(rent);
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = Double.toString(size);
+    }
 }
